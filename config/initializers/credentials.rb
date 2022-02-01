c45f0f77cfbc96d314dc635aa6b189327e6dd2b3
Dale Zak <dalezak@gmail.com>
2022-02-01 09:27:46 -0600

    Added credentials initializer

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -1,4 +1,11 @@
 Rails.application.credentials.config.each do |key, value|
-  ENV[key.to_s.upcase] = value.to_s
-  ENV[key.to_s.downcase] = value.to_s
+  if key.to_s == Rails.env
+    value.each do |env_key, env_value|
+      ENV[env_key.to_s.upcase] = env_value.to_s if ENV[env_key.to_s.upcase].blank?
+      ENV[env_key.to_s.downcase] = env_value.to_s if ENV[env_key.to_s.downcase].blank?
+    end  
+  else
+    ENV[key.to_s.upcase] = value.to_s if ENV[key.to_s.upcase].blank?
+    ENV[key.to_s.downcase] = value.to_s if ENV[key.to_s.downcase].blank?
+  end
 end
