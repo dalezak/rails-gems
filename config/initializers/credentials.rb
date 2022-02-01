8991d0384b62a60d2c6d5c7634d02095b69962c9
Dale Zak <dalezak@gmail.com>
2022-02-01 09:34:53 -0600

    Updated credentials initializer

--- config/initializers/credentials.rb
+++ config/initializers/credentials.rb
@@ -4,7 +4,7 @@ Rails.application.credentials.config.each do |key, value|
       ENV[env_key.to_s.upcase] = env_value.to_s if ENV[env_key.to_s.upcase].blank?
       ENV[env_key.to_s.downcase] = env_value.to_s if ENV[env_key.to_s.downcase].blank?
     end  
-  else
+  elsif ['development', 'staging', 'test', 'production'].include?(key.to_s) == false
     ENV[key.to_s.upcase] = value.to_s if ENV[key.to_s.upcase].blank?
     ENV[key.to_s.downcase] = value.to_s if ENV[key.to_s.downcase].blank?
   end
