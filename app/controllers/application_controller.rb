901b02f4fb02f0729bbce9f1b0806a5f40936616
Dale Zak <dalezak@gmail.com>
2022-02-11 14:25:52 -0600

    Added title to signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -92,8 +92,8 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :title])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :title])
   end
 
 end
