876647387d51cdd897e7975cd830a57e96ba0bce
Dale Zak <dalezak@gmail.com>
2022-02-14 14:22:49 -0600

    Updated sitemap_generator

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -7,13 +7,16 @@ if Rails.env.production?
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
     region: ENV['AWS_REGION'])
   SitemapGenerator::Sitemap.create do
-    add home_path, priority: 0.7, changefreq: 'weekly'
+    add home_path, priority: 0.9, changefreq: 'weekly'
+    add gems_path, priority: 0.8, changefreq: 'weekly'
     Gemm.find_each do |gemm|
       add gem_path(gemm), priority: 0.8, changefreq: 'weekly', lastmod: gemm.updated_at
     end
+    add users_path, priority: 0.8, changefreq: 'weekly'
     User.find_each do |user|
-      add user_path(user), priority: 0.9, changefreq: 'weekly', lastmod: user.updated_at
+      add user_path(user), priority: 0.8, changefreq: 'weekly', lastmod: user.updated_at
     end
+    add tags_path, priority: 0.6, changefreq: 'weekly'
     Tag.find_each do |tag|
       add tag_path(tag), priority: 0.6, changefreq: 'monthly', lastmod: tag.updated_at
     end
